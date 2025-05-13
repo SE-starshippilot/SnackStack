bdb44f92d2a515450a64a1ad3feb30292a96b191
Tianhao <120090472@link.cuhk.edu.cn>
2025-05-13 13:10:14 -0400

    feat: implemented front end code for displaying generated recipes using backend provided data

--- client/src/types/recipe.ts
+++ client/src/types/recipe.ts
@@ -1,18 +1,20 @@
-export interface RecipeIngredient {
-  ingredient_name: string;
+export interface Ingredient {
+  ingredientName: string;
   quantity: number;
-  unit: string;
+  unit?: string;
   note?: string;
 }
 
 export interface Recipe {
-  id: string;
-  recipe_name: string;
+  uuid: string;
+  recipeName: string;
   servings: number;
   description: string;
-  origin_name: string;
-  recipe_ingredients: RecipeIngredient[];
-  recipe_steps: string[];
-  date: string;
-  isFavorite?: boolean;
+  originName: string;
+  recipeIngredients: Ingredient[];
+  recipeSteps: string[];
+}
+
+export interface LocationState {
+  recipes: Recipe[];
 }
