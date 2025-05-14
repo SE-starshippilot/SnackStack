11281dc368b9927a1377b612fc1f3413c9a2b19e
pandapandabra <mariia_sidorova@brown.edu>
2025-05-13 21:07:58 -0400

    test: tests for login. Protected Routes/updated auth tests

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -2,7 +2,6 @@ interface ImportMetaEnv {
   VITE_DISABLE_AUTH: string;
   VITE_CLERK_PUBLISHABLE_KEY: any;
   VITE_APP_NODE_ENV: string;
-  // define more env variables if needed
 }
 
 interface ImportMeta {
